58f3fc997e21567d53b4d902a0bdcd20dae22526
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-03-31 02:25:20 -0300

    Update health_check.sh

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,7 +1,55 @@
 #!/bin/bash
 
 echo "===== HEALTH CHECK ====="
+
+echo "Hostname:"
 hostname
+
+echo "Date:"
+date
+
+OS=$(uname)
+
+echo "OS Detected: $OS"
+
+echo "----------------------"
+
+# DISK
+echo "[DISK USAGE]"
+if [ "$OS" = "Linux" ]; then
+    df -h
+else
+    df -g
+fi
+
+echo "----------------------"
+
+# MEMORY
+echo "[MEMORY USAGE]"
+if [ "$OS" = "Linux" ]; then
+    free -m
+elif [ "$OS" = "AIX" ]; then
+    vmstat -s
+else
+    echo "Memory command not standardized for this OS"
+fi
+
+echo "----------------------"
+
+# CPU
+echo "[CPU INFO]"
+if [ "$OS" = "Linux" ]; then
+    lscpu | grep "Model name"
+elif [ "$OS" = "AIX" ]; then
+    lsattr -El proc0 | grep frequency
+else
+    echo "CPU info command not standardized"
+fi
+
+echo "----------------------"
+
+# UPTIME
+echo "[UPTIME]"
 uptime
-df -h
-free -m
+
+echo "======================"
